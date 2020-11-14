db353e0dde9bee75006c5a26494e0f4cde6764cf
Alexander <gwartneydev@gmail.com>
2020-11-13 20:49:24 -0800

    Refactored more fetch methods
    Refactored the structure of promises in the returns of fetch method.

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 
-let zipcode = 97420;
+let zipcode = "Coos Bay OR";
+let locations; 
 
-fetch(`https://api.opencagedata.com/geocode/v1/json?q=${zipcode}&key=96284b3b928a4567aed3c32ec2e0ab41`)
-.then(response=>{
-  return response.json();
-}).then(data=>{
+function getUserInput(){
+	
+ return; 
+}
+
+async function getLocation(userlocation){
+    let userInput = getUserInput();
+	let response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${userInput}&key=96284b3b928a4567aed3c32ec2e0ab41`);
+	let data =  await response.json();
+	return data;
+}
+
+async function setLocation(data){
 
   let latatitude = data.results[0].geometry.lat;
   let longaitude = data.results[0].geometry.lng;
+  
+  let userLocation = await fetch(`https://weather-proxy.freecodecamp.rocks/api/current?lat=${latatitude}&lon=${longaitude}`)
 
-  return fetch(`https://weather-proxy.freecodecamp.rocks/api/current?lat=${latatitude}&lon=${longaitude}`)
+  let jsonresponse = await userLocation.json();
+  
+  return jsonresponse;
+}
 
-}).then(response =>{
-  return response.json();
-}).then(data=>{
-  console.log(data);
+getLocation(zipcode)
+
+.then(data=>{
+ 
+ setLocation(data)
+
+   .then(data=>{
+   		
+	})
 })
+
+
